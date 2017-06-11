f16269c7ee95c9b4dfff5759c822b328aefde05a
Dave Ritchie <ritchiedw@hotmail.com>
2017-06-11 14:03:28 +0100

    working my way through this example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,45 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent} from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+//import { RouterModule } from '@angular/router';
+import { DashboardComponent} from './dashboard.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    FormsModule
+    //HeroDetailComponent,
+/*    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+*/
+      //{
+    //    path: 'detail/:id',
+    //    component: HeroDetailComponent
+    //  }
+    //])
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
+
+
 export class AppModule { }
